9e9e3ab8eecee385be02215006202fb32cbc125f
Berk <berk@monster.localdomain>
2018-01-14 18:55:25 +0300

    Vertex and indexes

--- main.cpp
+++ main.cpp
@@ -18,6 +18,47 @@ static void errorCallback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
+void renderFunction()
+{
+	int width = 4;
+	int height = 2;
+	for (int i=0; i<=height; i++)
+	{
+		for (int j=0; j<=width; j++)
+		{
+			int xpos = j;
+			int zpos = height - i;
+			std::cout << "For vertex (" << j << "," << i << ") : x = " << xpos << ", z = " << zpos << std::endl;
+		}
+	}
+	std::cout << std::endl;
+		std::cout << 0 << std::endl;
+
+	for (int j=0; j<height; j++)
+	{
+		std::cout << (j+1) * (width+1) << std::endl << j * (width+1) + 1 << std::endl;
+		for (int i=1; i<=width; i++)
+		{
+		    if (i != width)
+				std::cout << (j+1) * (width+1) + i << std::endl << j * (width+1) + i + 1 << std::endl;
+			else if (j != height - 1)
+			{
+				std::cout << (j+1) * (width+1) + i << std::endl;
+				std::cout << (j+1) * (width+1) + i << std::endl;
+				std::cout << (j) * (width+1) + i + 1 << std::endl;
+				std::cout << (j) * (width+1) + i + 1 << std::endl;
+			}
+			else
+			{
+				std::cout << (j+1) * (width+1) + i << std::endl;
+
+
+			}
+		}
+	}
+
+}
+
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
@@ -33,7 +74,7 @@ int main(int argc, char *argv[]) {
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
 
   win = glfwCreateWindow(600, 600, "CENG477 - HW4", NULL, NULL);
 
@@ -56,10 +97,8 @@ int main(int argc, char *argv[]) {
   initTexture(argv[1], &widthTexture, &heightTexture);
 
 
-
+  renderFunction();
   while(!glfwWindowShouldClose(win)) {
-
-
     glfwSwapBuffers(win);
     glfwPollEvents();
 
